f2b1cc9da2f358a640b2dee4e12099467ea294f5
akaRoman212 <101047114+akaRoman212@users.noreply.github.com>
2022-04-06 11:05:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,8 @@
 import asyncio
+import telebot
+from telethon.sync import TelegramClient
+from telethon.tl.types import InputPeerUser, InputPeerChannel
+from telethon import TelegramClient, sync, events
 
 from kucoin.client import Client
 import pprint 
@@ -12,13 +16,27 @@ freq = 440  # Hz
 # api_secret = '1b104eeb-4d9e-40c5-9dc7-51967902b0e5'
 # api_passphrase = '00000000'
 # client = Client(api_key, api_secret, api_passphrase, sandbox=True)
-
-api_key = 
-api_secret = 
-api_passphrase = 
+api_id = '12788011'
+api_hash = 'f06ac3603456859eccfd831b62d8134a'
+api_key = '6224bde70fd8a700019e8bc5'
+api_secret = '327de38a-60ad-4606-898d-50f03d50c680'
+api_passphrase = '85462500'
 
 client = Client(api_key, api_secret, api_passphrase)
+phone = '+917351699598'
+  
+# creating a telegram session and assigning
+# it to a variable client
+tclient = TelegramClient('bin-session', api_id, api_hash)
+  
+# connecting and building the session
+tclient.connect()
+if not tclient.is_user_authorized():
 
+    tclient.send_code_request(phone)
+    
+    # signing in the client
+    tclient.sign_in(phone, input('Enter the code: '))
 
 tokens = psym.sym
 # pprint.pprint(currencies)
@@ -50,8 +68,18 @@ while True:
         for y in range(len(up_token)):
             if up_token[y] not in tokens:
                 print(up_token[y])
-                webbrowser.open("https://www.https://www.kucoin.com/news/categories/listing")
-        
+                webbrowser.open("https://www.kucoin.com/news/categories/listing")
+                try:
+                    
+
+                    # sending message using telegram client
+                    tclient.send_message('kinggamer212', up_token[y])
+                except Exception as e:
+                    
+                    # there may be many error coming in while like peer
+                    # error, wrong access_hash, flood_error, etc
+                    print(e)
+
     
     print("Kucoin Searching..............")
     time.sleep(60)
